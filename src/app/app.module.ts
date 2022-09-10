d9944eba77f2e490d9511e19d939263d142c8be3
Dilyar <dilyar.iskan@rwth-aachen.de>
2022-09-10 02:46:19 +0200

    navbar and carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OffersComponent } from './components/offers/offers.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OffersComponent,
+    NavbarComponent,
+    AboutComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
